19a94be3bf7c61d2aeeee8279850d65e366d2626
feaad <antwi-donkor_f@outlook.com>
2023-11-05 18:52:33 +0000

    encoder function

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ def encrypt(plaintext: str, bit_op: str, key: str):
         left = plaintext[:int(len(plaintext)/2)]
         right = plaintext[int(len(plaintext)/2):]
         
+        encoder(bit_op, key, right)
+        # print(right)
         
+
+def encoder(bit_op: str, key: str, right: str):
+    if bit_op == 'AND':
+        encode = logic.and_op(key, right) 
+        print(encode)
+
+
+encrypt('10010011', 'AND', '1101')
