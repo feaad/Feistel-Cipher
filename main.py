d3abe9155a94c4d90d6ad708d0e2ac940aa1a296
feaad <antwi-donkor_f@outlook.com>
2023-11-07 11:05:22 +0000

    refactoring

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ Copyright ©2023 Frances Antwi-Donkor
 import utils.logic_gate as logic
 
 
-def encrypt(plaintext: str, bit_op: str, key: str):   
-    '''Encrypt takes in 3 parameters to encrypt binary code'''
+def encrypt(plaintext: str, bit_op: str, key: str):
+    """Encrypt takes in 3 parameters to encrypt binary code"""
     right_one = ""
     if len(plaintext) % 2 != 0:
         print("Input must even")
     else:
         left = plaintext[: int(len(plaintext) // 2)]
-        right = plaintext[int(len(plaintext) // 2):]
+        right = plaintext[int(len(plaintext) // 2) :]
 
         end = encoder(bit_op, key, right)
         right_one = xor_left(end, left)
@@ -31,7 +31,7 @@ def encrypt(plaintext: str, bit_op: str, key: str):
 
 
 def encoder(bit_op: str, key: str, right: str):
-    '''Creates an encoder with respect to the logic gates'''
+    """Creates an encoder with respect to the logic gates"""
     if bit_op == "AND":
         return logic.and_op(key, right)
     elif bit_op == "OR":
@@ -41,7 +41,7 @@ def encoder(bit_op: str, key: str, right: str):
 
 
 def xor_left(left: str, encode: str):
-    '''An xor function to obtain the right_one binary code'''
+    """An xor function to obtain the right_one binary code"""
     return logic.xor_op(encode, left)
 
 
