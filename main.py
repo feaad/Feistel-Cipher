5f20249f52a2b9e8c87239cc7d8f51efaea9c153
feaad <antwi-donkor_f@outlook.com>
2023-11-07 11:26:26 +0000

    Feistel decryption algorithm

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ Copyright ©2023 Frances Antwi-Donkor
 import utils.logic_gate as logic
 
 
-def encrypt(plaintext: str, bit_op: str, key: str):
-    """Encrypt takes in 3 parameters to encrypt binary code"""
+def encrypt(plaintext: str, bit_op: str, key: str) -> str:
+    """The function encrypt() takes in a key and a logic operand to encrypt
+    binary code"""
+
     right_one = ""
     if len(plaintext) % 2 != 0:
         print("Input must even")
@@ -25,12 +27,26 @@ def encrypt(plaintext: str, bit_op: str, key: str):
         left = plaintext[: int(len(plaintext) // 2)]
         right = plaintext[int(len(plaintext) // 2) :]
 
-        end = encoder(bit_op, key, right)
-        right_one = xor_left(end, left)
+        encrypter = encoder(bit_op, key, right)
+        right_one = xor(encrypter, left)
     return f"{right}{right_one}"
 
 
-def encoder(bit_op: str, key: str, right: str):
+def decrypt(cipher_text: str, bit_op: str, key: str) -> str:
+    """The function decrypt() takes in a key and a logic operand to decrypt
+    binary code"""
+    if len(cipher_text) % 2 != 0:
+        print("Ciphertext should be even")
+    else:
+        left_one = cipher_text[: int(len(cipher_text) // 2)]
+        right_one = cipher_text[int(len(cipher_text) // 2) :]
+
+        decrypter = encoder(bit_op, key, left_one)
+        left = xor(right_one, decrypter)
+    return f"{left}{left_one}"
+
+
+def encoder(bit_op: str, key: str, right: str) -> str:
     """Creates an encoder with respect to the logic gates"""
     if bit_op == "AND":
         return logic.and_op(key, right)
@@ -40,9 +56,6 @@ def encoder(bit_op: str, key: str, right: str):
         return logic.xor_op(key, right)
 
 
-def xor_left(left: str, encode: str):
+def xor(binary_bit: str, encode: str) -> str:
     """An xor function to obtain the right_one binary code"""
-    return logic.xor_op(encode, left)
-
-
-encrypt("10010011", "AND", "1101")
+    return logic.xor_op(encode, binary_bit)
